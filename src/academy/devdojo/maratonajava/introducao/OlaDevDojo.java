cb926c3d6de7a8bd5ebfc0008e3d5e5887792a05
alfmorais <alfredneto@hotmail.com>
2024-03-16 15:55:29 -0300

    Feat: introduction about comments in Java

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso e um cometario <b>JavaDoc</b>
+ */
 public class OlaDevDojo {
+    // Isso e um comentario de uma linha
+    /*
+        Isso tambem e um comentario
+        de varias linhas
+     */
     public static void main(String[] args) {
         System.out.println("Hello World Dev Dojo!");
         System.out.println("KA-ME-HA-ME-HA");
