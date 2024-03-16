d71ea0d00dffe7f21df3bbab46c40bb1adc91695
alfmorais <alfredneto@hotmail.com>
2024-03-16 15:39:27 -0300

    Feat: first setup and hello world code

---
+++ src/OlaDevDojo.java
@@ -0,0 +1,6 @@
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Hello World Dev Dojo!");
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
