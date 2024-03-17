83175357af054881bb16bf87a516609fe8c6b518
alfmorais <alfredneto@hotmail.com>
2024-03-17 14:52:21 -0300

    Feat: primitive types

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,10 +3,32 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         /*
-        8 Tipos Primitivos:
-        -> int, double, float, char, byte, short, long, boolean
+        Tipos Primitivos:
+            1) int
+            2) double
+            3) float
+            4) char
+            5) byte
+            6) short
+            7) long
+            8) boolean
          */
         int age = 31;
-        System.out.println("idade: " + age);
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        char caracter = '\u0041';
+        byte ageByte = 127;
+        short ageShort = 10;
+        long bigNumber = 10_000_000;
+        boolean isMoreThanEighteen = false;
+
+        System.out.println("Idade (int): " + age);
+        System.out.println("Salario (double): " + salaryDouble);
+        System.out.println("Salario (float): " + salaryFloat);
+        System.out.println("Letra (char): " + caracter);
+        System.out.println("Idade (byte): " + ageByte);
+        System.out.println("Idade (short): " + ageShort);
+        System.out.println("Grande Número (long): " + bigNumber);
+        System.out.println("Maior de 18 (boolean): " + isMoreThanEighteen);
     }
 }
